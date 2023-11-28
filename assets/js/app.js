fe71bfe34533f131e380f71e84adce92c867b281
dtsitovich <wasborn2trip@gmail.com>
2023-11-28 15:14:13 +0100

    feat: added functions loadData() and saveData() for reading/writing local storage data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('Hello world!');
+let habits = [];
+const HABIT_KEY = 'HABIT_KEY';
+
+/* Local Storage */
+
+function loadData() {
+    const dataFromStorage = localStorage.getItem('HABIT_KEY');
+    const habitArr = JSON.parse(dataFromStorage);
+    if (Array.isArray(habitArr)) {
+        habits = habitArr;
+    }
+}
+
+function saveData() {
+    localStorage.setItem('HABIT_KEY', JSON.stringify(habits));
+}
+
+// init
+(() => {
+    loadData();
+})()
